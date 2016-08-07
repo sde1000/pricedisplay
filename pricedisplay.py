653afa949aa21b0a6b5177f46dc7decf0e9b6206
Stephen Early <steve@greenend.org.uk>
2016-08-07 15:35:07 +0100

    XXX EMF We got volunteers!

--- pricedisplay.py
+++ pricedisplay.py
@@ -48,8 +48,8 @@ def repaint(d):
             s = f.render("Network error - not up to date", True, (255, 0, 0))
             d.blit(s, (0, 0))
             pygame.display.update()
-    lines.insert(0, ((0xff,0xff,0x00),"","We need more volunteers today!",""))
-    lines.append(((0x00,0xff,0x00),"","Please go to volunteer.emfcamp.org",""))
+    #lines.insert(0, ((0xff,0xff,0x00),"","We need more volunteers today!",""))
+    #lines.append(((0x00,0xff,0x00),"","Please go to volunteer.emfcamp.org",""))
     fontsize = d.get_height() // len(lines)
     lineheight = fontsize
     widths = [width + 1]
